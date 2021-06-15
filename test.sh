22d299307302a5b25fb086ae610118833b92352b
HaiShaw <hixiao@gmail.com>
2021-06-15 15:40:55 -0400

    add TF2 compat support and remove tf.contrib

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Inference Example
+python test_lanenet.py --weights_path /dockerx/lanenet-lane-detection/model/tusimple_lanenet.ckpt --image_path ./data/tusimple_test_image/0.jpg
---
+++ train.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Train using config/tusimple_lanenet.yaml
+python train_lanenet_tusimple.py
